489ac899255d20d62a3354184069c084aab3d880
diego-teixeira-souza <diego.teixeira.souza@hotmail.com>
2022-03-13 14:36:22 -0300

    000000001 - Hello World

---
+++ src/helloworld/helloworldGUI.asm
@@ -0,0 +1,32 @@
+; ----------------------------------------------------------------------------------------------------
+; file: helloworldGUI.asm
+; description: A simple program in 32 bits that display in GUI an Hello World!
+; ----------------------------------------------------------------------------------------------------
+
+%define NULL 0
+%define MB_OK 0
+%define EXIT_CODE 0
+
+section .info									; Linker section
+	global	_main								; Define the main entry point
+	extern	_MessageBoxA@16						; External Win32API function
+    extern	_ExitProcess@4						; External Win32PAI function
+
+section .text									; Code section
+	_main:										; Start of Main Program
+		.prompt:
+		push	DWORD MB_OK						; UINT uType
+		push	DWORD tit						; LPCSTR lpCaption
+		push 	DWORD msg						; LPCSTR lpText
+		push	DWORD NULL						; HWND hWnd
+		call 	_MessageBoxA@16					; Display an prompt GUI
+		.done:
+		push	dword EXIT_CODE					; UINT uExitCode
+		call	_ExitProcess@4					; Windows Exit Process Function
+		
+section .data									; Initialized variables area
+	tit db "Example in GUI", NULL				; Prompt Title
+	msg db "Hello World!", NULL					; Hello World message
+; ----------------------------------------------------------------------------------------------------
+; compiler: nasm -fwin32 -ohelloworldGUI.obj helloworldGUI.asm && gcc -m32 -mwindows -ohelloworldGUI.exe helloworldGUI.obj
+; ----------------------------------------------------------------------------------------------------
